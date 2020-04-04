9d11d445f0c06b4316dd5250f37d77fdcec0c96e
罗斌 <dustdawn@163.com>
2020-04-04 11:41:32 +0800

    ITMooc 文件服务bug修改

--- it-service-filesystem/src/main/java/com/njit/filesystem/FileSystemApplication.java
+++ it-service-filesystem/src/main/java/com/njit/filesystem/FileSystemApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableDiscoveryClient //EurekaClient能在EurekaServer被发现服务
 @SpringBootApplication//扫描所在包及子包的bean，注入到ioc中
-@EntityScan("com.xuecheng.framework.domain.filesystem")//扫描实体类
+@EntityScan("com.njit.framework.domain.filesystem")//扫描实体类
 @ComponentScan(basePackages={"com.njit.api"})//扫描接口
 @ComponentScan(basePackages={"com.njit.framework"})//扫描framework中通用类
 @ComponentScan(basePackages={"com.njit.filesystem"})//扫描本项目下的所有类
