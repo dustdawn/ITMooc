f918664eb3c13f8046e3ca652ed099bf7ad7865f
罗斌 <dustdawn@163.com>
2020-04-20 16:06:22 +0800

    ITMooc 服务列表清单修改

--- it-framework-common/src/main/java/com/njit/framework/client/MoocServiceList.java
+++ it-framework-common/src/main/java/com/njit/framework/client/MoocServiceList.java
@@ -10,25 +10,26 @@ public class MoocServiceList {
     //Eureka服务注册中心32000/320001
     public static final String MOOC_GOVERN_CENTER = "mooc-govern-center";
     //CMS服务31100
-    public static final String MOOC_SERVICE_MANAGE_CMS = "mooc-service-cms";
+    public static final String MOOC_SERVICE_CMS = "mooc-service-cms";
     //课程管理服务31200
-    public static final String MOOC_SERVICE_MANAGE_COURSE = "mooc-service-course";
+    public static final String MOOC_SERVICE_COURSE = "mooc-service-course";
     //用户中心服务31300
     public static final String MOOC_SERVICE_UCENTER = "mooc-service-ucenter";
     //媒资服务31400
-    public static final String MOOC_SERVICE_MANAGE_MEDIA = "mooc-service-media";
+    public static final String MOOC_SERVICE_MEDIA = "mooc-service-media";
     //图片文件系统服务31500
-    public static final String MOOC_SERVICE_BASE_FILESYSTEM = "mooc-service-filesystem";
+    public static final String MOOC_SERVICE_FILESYSTEM = "mooc-service-filesystem";
     //搜索服务31600
     public static final String MOOC_SERVICE_SEARCH = "mooc-service-search";
     //学习中心服务31700
     public static final String MOOC_SERVICE_LEARNING = "mooc-service-learning";
+    //选课服务31750
+    public static final String MOOC_SERVICE_SELECTION = "mooc-service-selection";
     //网关服务31800
     public static final String MOOC_GOVERN_GATEWAY = "mooc-govern-gateway";
     //认证服务31900
     public static final String MOOC_SERVICE_UCENTER_AUTH = "mooc-service-ucenter-auth";
-    //选课服务32100
-    public static final String MOOC_SERVICE_MANAGE_ORDER = "mooc-service-manage-order";
+
 
 
     public static final String MOOC_SERVICE_PORTALVIEW = "mooc-service-portalview";
