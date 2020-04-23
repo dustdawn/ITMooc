81dfd431d88a181e507dc22e3ec6ca30f988bd2d
罗斌 <dustdawn@163.com>
2020-04-23 23:09:26 +0800

    ITMooc api修改

--- it-service-api/src/main/java/com/njit/api/learning/CourseLearningControllerApi.java
+++ it-service-api/src/main/java/com/njit/api/learning/CourseLearningControllerApi.java
@@ -1,6 +1,9 @@
 package com.njit.api.learning;
 
+import com.njit.framework.domain.learning.LearningCourse;
+import com.njit.framework.domain.learning.request.LearningCourceRequest;
 import com.njit.framework.domain.learning.response.GetMediaResult;
+import com.njit.framework.model.response.QueryResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -18,4 +21,14 @@ public interface CourseLearningControllerApi {
      */
     @ApiOperation("获取课程学习地址")
     public GetMediaResult getmedia(String courseId, String teachplanId);
+
+    /**
+     * 根据用户id查询课程列表
+     * @param page
+     * @param size
+     * @param learningCourseRequest
+     * @return
+     */
+    @ApiOperation("查询课程列表")
+    public QueryResponseResult<LearningCourse> findList(int page, int size, LearningCourceRequest learningCourseRequest);
 }
